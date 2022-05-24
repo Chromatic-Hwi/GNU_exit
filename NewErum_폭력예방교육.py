6767325d7f55ab855efb3dfc7810ae99ee9f7e1b
Seo Hwi <chromatic_365@naver.com>
2022-05-24 13:43:12 +0900

    220524 주석 삭제 및 코드 정리

--- NewErum_폭력예방교육.py
+++ NewErum_폭력예방교육.py
@@ -38,7 +38,7 @@ driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/aside/nav/ul/li[2]/p/a').
 driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/aside/nav/ul/li[2]/div/ul/li[2]/a').click()
 
 
-# 폭력예방교육 버튼 찾기
+
 N=1
 while(True):
     try:
@@ -52,30 +52,26 @@ while(True):
             print("폭력예방교육을 신청하지 않으신 것 같습니다.")
             driver.quit()
 
-# 폭력 예방 교육 [강의보기] 클릭
 driver.find_element(By.XPATH,'//*[@id="tilesContent"]/div[2]/table/tbody/tr['+str(N)+']/td[7]/a').click()
 time.sleep(0.5)
 
-# 현재 열린 탭들의 주소를 리스트로 반환, 그중 두번째 즉 새로 열린 탭을 선택하기 위함.
 Tab_Address = driver.window_handles
 time.sleep(0.5)
-# 탭 전환
 driver.switch_to.window(Tab_Address[1])
 time.sleep(0.5)
 driver.find_element(By.XPATH,'//*[@id="sidenav"]/nav/ul/div/ul/li[2]/a').click()
 
-# 총 강의수는 25개
 for i in range(2, 27):
     CHECK = str(driver.find_element(By.XPATH,'//*[@id="learning_list"]/tbody/tr['+str(i)+']/td[6]/span').text)
     if CHECK=="출석완료":
         pass
     else:
         T=driver.find_element(By.XPATH,'//*[@id="learning_list"]/tbody/tr['+str(i)+']/td[4]').text
-        T=int(T[:-1]) # 강의 길이에 해당하는 분이 나옴.
-        T_Final=T*60+60 # 강의 시간 +10초를 통해 동작 여유시간을 줌
+        T=int(T[:-1]) 
+        T_Final=T*60+60 
         driver.find_element(By.XPATH,'//*[@id="learning_list"]/tbody/tr['+str(i)+']/td[7]/a[1]').click()
-        time.sleep(T_Final) # 강의가 재생될테니 sleep을 걸어줌
-        driver.find_element(By.XPATH,'//*[@id="btn_list"]').click() # 시간이 지나면 [목록] 클릭
+        time.sleep(T_Final) 
+        driver.find_element(By.XPATH,'//*[@id="btn_list"]').click()
     
-driver.quit() # 학습 완료후 종료
+driver.quit()
 
